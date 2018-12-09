d80ff5c82fa185c7e4545431891e5cf33f92ded0
ElenaNovakova <elena.nova06>
2018-12-09 22:33:59 +0100

    faceNotebook modified

--- frontEnd/javaScript/exercises/object facenotebook/event.js
+++ frontEnd/javaScript/exercises/object facenotebook/event.js
@@ -1,7 +1,6 @@
 function Event(name) {
-    // have name date description and users
     this.name = "event " + name;
     this.date = new Date();
     this.discription = " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat nemo earum tempore, dolorem laborum ipsa. Ipsum repellat nam veritatis cupiditate incidunt, dolores possimus enim vel non, quaerat numquam minus nisi!"
-    this.userList = [];
+    this.userList = [];  
 }
--- frontEnd/javaScript/exercises/object facenotebook/social-network.js
+++ frontEnd/javaScript/exercises/object facenotebook/social-network.js
@@ -2,43 +2,52 @@ function SocialNetwork() {
     this.users = [];
     this.events = [];
 
-
     this.generateUsers = function () {
-        for (let i = 1; i <= 50; i++) {
+        for (let i = 0; i < 50; i++) {
             var user = new User(i);
             this.users.push(user);
         };
     };
 
     this.generateEvents = function () {
-        for (let i = 1; i <= 5; i++) {
+        for (let i = 0; i < 5; i++) {
             var event = new Event(i);
             event.userList.push(i)
             this.events.push(event);
         };
-    };
+    }; 
 
-    this.userChoosEvent = function () {
+    this.eventChoosUser = function () {
         for (let i = 0; i < this.users.length; i++) {
-            for (let ј = 0; ј < 3; ј++) {
-                var userEvent = getRandom(0, 5);
-                this.events[userEvent].userList.push(this.users[i]);
+            var userEvent = getRandom(0, 5);
+            this.events[userEvent].userList.push(this.users[i]);
+            for (let j = 0; j < 3; j++) {
+                this.sendEventRequest(i);
             }
         }
     };
 
-    this.addFriend = function () {
-        for (let i = 1; i < this.users.length; i++){
-                var userFriend = getRandom(0, this.users.length - 1);
-                this.users[userFriend].friendList.push(this.users[i]);
+    this.sendEventRequest = function(i){
+        var userEvent = getRandom(0, 5);
+        if (!this.events[userEvent].userList.includes(this.users[i])) {
+            this.events[userEvent].userList.push(this.users[i]);
         }
     }
 
-    // da se podobri: da nema isti user vo eden event i da sekoj user ima max 5 prijateli
+    this.addFriend = function () {
+        for (let i = 0; i < this.users.length; i++) {
+            var userFriend = getRandom(1, this.users.length);
+            this.users[userFriend].friendList.push(this.users[i]);
+            this.users[i].friendList.push(this.users[userFriend]);
+            if (this.users[userFriend].friendList.length > 5) {
+                break;
+            }
+        }
+    }
 
     this.generateEvents();
     this.generateUsers();
-    this.userChoosEvent();
+    this.eventChoosUser();
     this.addFriend();
 }
 
