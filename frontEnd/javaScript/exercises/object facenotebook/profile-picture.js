c90bf5c3c949502d31b04c1473d0669e8d4ffa8a
ElenaNovakova <elena.nova06>
2018-12-10 22:19:20 +0100

    faceNotebook profile

--- frontEnd/javaScript/exercises/object facenotebook/profile-picture.js
+++
@@ -1,5 +0,0 @@
-function ProfilePictire() {
-    this.link = new URL ("https://www.google.com/");
-    this.altText = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero nihil doloribus.";
-    this.flag = true;
-}
